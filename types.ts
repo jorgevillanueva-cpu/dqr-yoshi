db6f1fbb981376407e320951275c1858e879c3d4
Jorge Villanueva <jorge.villanueva@boletomovil.com>
2026-01-13 19:33:29 -0600

    update

--- types.ts
+++ types.ts
@@ -3,12 +3,5 @@ export interface TicketData {
   saldo: string;
   codigo: string;
   phone: string;
-}
-
-export interface PromoData {
-  tipo: string;
-  producto?: string;
-  promo: string;
-  phone: string;
-  index: number;
+  logoUrl?: string;
 }
